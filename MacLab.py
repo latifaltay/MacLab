5f7b9e940fa88b7503ad5ddc22d103b7695e354f
Latif Altay <latifaltay00@gmail.com>
2023-12-01 01:02:26 +0300

    first module is complated

--- MacLab.py
+++ MacLab.py
@@ -3,54 +3,109 @@ import re
 import random
 from scapy.all import *
 
+
+# MAC ADRESİ İŞLEMLERİ 
+#####################################################################################################################################################################################################
+
+#random mac adresi üretmemizi sağlayan fonksiyon
+def get_random_mac_address():
+    
+    mac_parts = []
+
+    for _ in range(6):
+        # random kütüphanesi yardımıyla 0 ile 255 arasında bir byte seçiliyor
+        random_byte = random.randint(0x00, 0xff)
+
+        # seçilen byte'ı onaltılık formata çeviriyor 
+        hex_byte = format(random_byte, '02x')
+
+        # onaltılık formata çevrilen byte gruplarını mac_parts listesine ekliyoruz
+        mac_parts.append(hex_byte)
+
+    # onaltılık formata çevrilen byte gruplarını : işareti ile birleştirip mac adresi oluşturuyoruz
+    mac_address = ':'.join(mac_parts)
+
+    return mac_address
+
+
+# mac adresi değiştirmeyi sağlayan fonksiyon
+# kısaca subprocess kütüphanesi aracılığıyla aşağıdaki linux komutlarını çalıştırarak new_mac değişkenine atanılan random mac adresini gerçek mac adresi yerine atıyor 
+def change_mac_address(interface, new_mac):
+    subprocess.call(["sudo", "ifconfig", interface, "down"])
+    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
+
+# Mac adresinin değiştirildiğini yardıran fonksiyon
+def change_mac_and_print(interface):
+    new_mac_address = get_random_mac_address()
+    change_mac_address(interface, new_mac_address)
+    print(f"\nMAC adresi başarıyla değiştirildi. Yeni MAC adresi: {new_mac_address}")
+
+#####################################################################################################################################################################################################
+
+
+
+# iNTERFACE İŞLEMLERİ
+#####################################################################################################################################################################################################
+
+# Network interface'leri listeyen fonksiyon
 def list_network_interfaces():
     try:
-        result = subprocess.check_output(['ifconfig'], universal_newlines=True)
+        # subprocess kütüphanesi'nin check_output özelliği ile ifconfig kodunu çalıştırıyoruz, universal_newlines=true ise ifconfig kodunun çıktısını string'e çeviriyor ve okumamızı sağlıyor
+        result = subprocess.check_output(['ifconfig'], universal_newlines = True)
         return result
-    except subprocess.CalledProcessError:
-        return "İnterface'ler listelenemedi."
+    except subprocess.CalledProcessError as error:
+        # Eğer ifconfig kodunu çalıştırdığımızda hata alıyorsak önce hatanın kodunu sonra çıktısını dönüyoruz 
+        return ("Hata Kodu: " + str(error.returncode), "\n Hata Çıktısı: " + str(error.output), " \nİnterface'ler Listelenemedi.")
+    
 
+# listelenen interface'lerden istediğimizi seçmemize olanak sağlayan fonksiyon
 def select_network_interface():
+    # interface'leri listeleyen fonksiyonu çağırıyoruz
     interfaces = list_network_interfaces()
     print("Mevcut İnterface'ler:")
+    
+    # list_network_interfaces fonksiyonunun çağırdığı sonuçları re kütüphanesi ile listeye atıyoruz
     interface_list = re.findall(r'^\S+', interfaces, re.MULTILINE)
+
+    # listelenen interface'leri ekrana yazdırıyoruz
     for i, interface in enumerate(interface_list, 1):
         print(f"{i}. {interface}")
 
+    
     while True:
         try:
+            # listelenen interfacelerden birini kullanıcıdan alıp selection değişkenine atıyoruz
             selection = int(input("İnterface seçin (1 - n): "))
-            if 1 <= selection <= len(interface_list):
+            # kullanıcıdan alınan input 1 ya da 1'den büyükse ve input interface'lerin sayısından küçükse ya da eşitse bu blok çalışır ve kullanıcının seçimine göre işlem yapmayı sağlar
+            if 1 <= selection and selection <= len(interface_list):
+                # diziler 0'dan başladığı için kullanıcının girdiği değerden 1 çıkartıyoruz ve kullanıcının seçmek istediği interface'nin indis numarasını bulup onu döndürüyoruz
                 return interface_list[selection - 1]
             else:
                 print("Geçersiz bir seçenek girdiniz. Lütfen doğru sayı girin.")
         except ValueError:
             print("Geçersiz bir giriş. Lütfen bir sayı girin.")
 
-def change_mac_address(interface, new_mac):
-    subprocess.call(["sudo", "ifconfig", interface, "down"])
-    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["sudo", "ifconfig", interface, "up"])
 
-def get_random_mac_address():
-    return ':'.join([format(random.randint(0x00, 0xff), '02x') for _ in range(6)])
-
+# random ip adresi üreten fonksiyon (Şu an kullanılmıyor ilerleyen özelliklerde eğer uygun olursa dahil edilecek)
 def generate_random_ip():
     ip = []
     for _ in range(4):
         ip.append(str(random.randint(0, 255)))
+    # ip dizisindeki 4 adet sayıyı . karakteri ile birleştir ve döndür
     return '.'.join(ip)
 
+# syn paketi yollayan fonksiyon
 def send_syn_packet(source_ip, target_ip, target_port):
+    # scapy kütüphanesinin bu özelliği sayesinde çıktıları ekranda görebiliyoruz bunun default değeri 2'dir.
+    # fakat biz syn paketlerinin çıktısını görmek istemediğimiz için bunu 0'a çekiyoruz
+    # long story short ekrana detay yazdırmak istiyorsak 2 yani default değer eğer ekrana detay yazdırmak istemiyorsak 0 yazıyoruz 
     conf.verb = 0
+    # paketi hangi ip'den hangi ip'ye, hangi port numarası üzerinden hangi bit'i yollayacağımızı ayarladığımız kod
     packet = IP(src=source_ip, dst=target_ip) / TCP(dport=target_port, flags="S")
     send(packet)
 
-def change_mac_and_print(interface):
-    new_mac_address = get_random_mac_address()
-    change_mac_address(interface, new_mac_address)
-    print(f"\nMAC adresi başarıyla değiştirildi. Yeni MAC adresi: {new_mac_address}")
-
+# syn paketini yolladığında ekrana yazdıran fonksiyon
 def send_syn_and_print():
     source_ip = input("Kaynak IP adresini girin: ")
     target_ip = input("Hedef IP adresini girin: ")
@@ -58,19 +113,37 @@ def send_syn_and_print():
     send_syn_packet(source_ip, target_ip, target_port)
     print(f"SYN paketi {target_ip}:{target_port} adresine gönderildi.")
 
+
+
+
+# UYGULAMANIN ANA MENÜSÜ
+#####################################################################################################################################################################################################
+
+# ana menü fonksiyonu
 def main_menu():
     print("\n--- MacLab ---")
     print("1. İnterfaceleri listele ve MAC adresini değiştir")
     print("2. SYN Paketi Gönder")
     print("3. Çıkış")
 
+#####################################################################################################################################################################################################
+
+
+
+
+# MAİN METOTU
+#####################################################################################################################################################################################################
 def main():
     while True:
         main_menu()
+        # programın ana menüsünde olan seçeneklerden birini seçmemize yardımcı olan fonksiyon
+        # seçtiğimiz değeri choice değişkenine atıyor ve işlem yapıyor
         choice = input("Yukarıdakilerden Birini Seçiniz (1-3): ")
 
         if choice == '1':
+            # eğer birinci seçeneği seçersek select_network_interface fonksiyonu çağırılıyor ve network interface'leri listeleniyor
             selected_interface = select_network_interface()
+            # ardından seçtiğimiz interface'nin mac adresine random oluşturulan mac adresi atanıyor
             change_mac_and_print(selected_interface)
         elif choice == '2':
             send_syn_and_print()
